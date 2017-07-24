152835a246b7dbd4a083dcc3be92a91d9fa9e793
Olga Izbiakova <oizbyakova@gmail.com>
2017-07-24 00:07:39 -0400

    2d game ship and asteroids

---
+++ app/src/main/java/com/example/oizbyakova/myapplication/Ship.java
@@ -0,0 +1,30 @@
+package com.example.oizbyakova.myapplication;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class Ship extends SpaceBody {
+
+    public Ship(Context context) {
+        bitmapId = R.drawable.ship;
+        // определяем начальные параметры
+        size = 5;
+        x = 7;
+        y = GameView.maxY - size - 1;
+        speed = (float) 0.2;
+
+        init(context); // инициализируем корабль
+    }
+
+    @Override
+    public void update() { // перемещаем корабль в зависимости от нажатой кнопки
+        if(MainActivity.isLeftPressed && x >= 0){
+            x -= speed;
+        }
+        if(MainActivity.isRightPressed && x <= GameView.maxX - 5){
+            x += speed;
+        }
+    }
+}
